940f290956b692d4281fef00252a3b41ac555f5e
suprai567 <suprai567@gmail.com>
2022-09-15 19:19:26 +0530

    changes in searchbook

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import Book from './entity/Book';
-import BookDetails from './entity/BookDetails';
 const API_URL ="http://localhost:8082/digitalbooks";
 @Injectable({
   providedIn: 'root'
@@ -17,12 +16,6 @@ export class BookService {
   searchBooks(author: String, category: String, price: number, publisher: String){
     return this.client.get(API_URL + "/books/search?category=" + category + 
     "&author=" + author + "&price=" + price + "&publisher=" + publisher);
-  }
-  // buyBook(bookDetails: BookDetails){
-  //   return this.client.get(API_URL + "/books/buy");
-  // }
-  getAllPurchaseBooks(emailId: String){
-    return this.client.get(API_URL + "/reader/books");
   }
 
   
--- src/app/searchbook/searchbook.component.ts
+++ src/app/searchbook/searchbook.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import Book from '../entity/Book';
-import { BookService } from   '../book.service'
+import { BookService } from '../book.service'
 
 @Component({
   selector: 'app-searchbook',
@@ -10,24 +10,28 @@ import { BookService } from   '../book.service'
 export class SearchbookComponent implements OnInit {
   todaysDate: Date = new Date();
   //author:Author = new Author (1,"Supriya",25,"sup123@gmail.com");
-  book:Book= new Book("Bhagat", "2States","comic","Rupa Publication",134,"Chetan",this.todaysDate,true);
-  categoryList: any = ['comic', 'horror', 'action', 'drama', 'adventure'];
-  books: any = []
-  category: String ='Comic';
-  author: String = 'Chetan';
-  price: number = 134;
-  publisher: String = 'Rupa Publication';
-  constructor(public bookService:BookService) { }
+  book: Book = new Book("Bhagat", "2States", "Comic", "Rupa Publication", 134, "Chetan", this.todaysDate, true);
+  books: any = [];
+  categoryList: any = ['Comic','Drama', 'Science','Romance','Detective','Horror','Adventure'];
+  //category: String = 'Comic';
+  //author: String = 'Chetan';
+  //price: number = 134;
+  //publisher: String = 'Rupa Publication';
+  constructor(public bookService: BookService) { }
 
   ngOnInit(): void {
   }
-  search(){
-    console.log("clicked");
-    const observable = this.bookService.searchBooks(this.author, this.category, this.price, this.publisher);
-    observable.subscribe((books)=>{
+  search() {
+    console.log("Book Searched");
+    const observable = this.bookService.searchBooks(this.book.author, this.book.category, this.book.price, this.book.publisher);
+    observable.subscribe((books) => {
       console.log(books);
       this.books = books;
+    },
+    (error) => {//error handler
+      alert('something went wrong inside searchBook');
     })
+    
   }
 
 }
